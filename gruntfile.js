b7c34d2bfc10ac57c03a3000e87c413f927627ae
vanstee <patrick@vanstee.me>
2014-04-03 11:25:25 -0400

    Concatenate javascripts to single source file

--- gruntfile.js
+++ gruntfile.js
@@ -7,15 +7,27 @@ module.exports = function(grunt) {
         }
       }
     },
+    concat: {
+      dist: {
+        files: {
+          'public/javascripts/script.js': 'assets/javascripts/**/*.js'
+        }
+      }
+    },
     watch: {
       stylesheets: {
         files: ['assets/stylesheets/**/*.scss'],
         tasks: ['sass'],
+      },
+      javascripts: {
+        files: ['assets/javascripts/**/*.js'],
+        tasks: ['concat'],
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('default', ['sass']);
